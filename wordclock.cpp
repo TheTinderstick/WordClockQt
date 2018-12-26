6da51c22c8b12e1ea70760caccb711846b9b7d5d
Stefan Singler <stefansingler@Stefans-MacBook-Pro.local>
2018-12-26 12:47:31 +0100

    German seems to work. Some useless lambdas included...

--- wordclock.cpp
+++ wordclock.cpp
@@ -4,7 +4,7 @@
 
 WordClock::WordClock()
 {
-  constructEnglish();
+  constructGerman();
   updateWords();
 }
 
@@ -15,9 +15,12 @@ void WordClock::updateWords()
   auto hours {theTime.hour()};
   qDebug() << "Time:" << theTime.toString();
   qDebug() << hours << ":" << minutes;
-  int minuteRange {minutes / 5};
-  qDebug() << "minuteRange:" << minuteRange;
 
+  auto timeInWords = update( hours, minutes );
+  for( int index : timeInWords )
+  {
+    qDebug() << clockWords.at(index).c_str();
+  }
 }
 
 void WordClock::constructEnglish()
@@ -49,14 +52,153 @@ void WordClock::constructEnglish()
 void WordClock::constructGerman()
 {
   clockWords.clear();
-  clockWords =
-  {
-    "es", "ist", "viertel", "zehn", "zwanzig",
-    "fuenf", "nach", "vor", "halb", "zwoelf",
-    "eins", "zwei", "drei", "vier", "fuenf",
-    "sechs", "elf", "sieben", "acht", "neun",
-    "zehn", "uhr"
+  clockWords = {
+    "es",
+    "ist",
+    "viertel",
+    "zehn",
+    "zwanzig",
+    "fuenf",
+    "nach",
+    "vor",
+    "halb",
+    "zwoelf",
+    "eins",
+    "zwei",
+    "drei",
+    "vier",
+    "fuenf",
+    "sechs",
+    "elf",
+    "sieben",
+    "acht",
+    "neun",
+    "zehn",
+    "uhr",
   };
+
+  auto getMinuteWords = [](uint8_t hours, uint8_t minutes) {
+    std::list<int> wordList;
+    int hourWordIndex = -1;
+    wordList.push_back(0); /* Es */
+    wordList.push_back(1); /* ist */
+    if(5 > minutes)
+    {
+      hourWordIndex = 2; /* X */
+      wordList.push_back(21); /* Uhr */
+    }
+    else if( 10 > minutes)
+    {
+      wordList.push_back(5); /* fuenf */
+      wordList.push_back(6); /* nach */
+      hourWordIndex = 4; /* X */
+    }
+    else if( 15 > minutes)
+    {
+      wordList.push_back(3); /* zehn */
+      wordList.push_back(6); /* nach */
+      hourWordIndex = 4; /* X */
+    }
+    else if( 20 > minutes)
+    {
+      wordList.push_back(2); /* viertel */
+      wordList.push_back(6); /* nach */
+      hourWordIndex = 4; /* X */
+    }
+    else if( 25 > minutes)
+    {
+      wordList.push_back(4); /* zwanzig */
+      wordList.push_back(6); /* nach */
+      hourWordIndex = 4; /* X */
+    }
+    else if( 30 > minutes)
+    {
+      wordList.push_back(5); /* fuenf */
+      wordList.push_back(7); /* vor */
+      wordList.push_back(8); /* halb */
+      hourWordIndex = 5; /* X+1 */
+    }
+    else if( 35 > minutes)
+    {
+      wordList.push_back(8); /* halb */
+      hourWordIndex = 3; /* X+1 */
+    }
+    else if( 40 > minutes)
+    {
+      wordList.push_back(5); /* fuenf */
+      wordList.push_back(6); /* nach */
+      wordList.push_back(8); /* halb */
+      hourWordIndex = 5; /* X+1 */
+    }
+    else if( 45 > minutes)
+    {
+      wordList.push_back(4); /* zwanzig */
+      wordList.push_back(7); /* nach */
+      hourWordIndex = 4; /* X+1 */
+    }
+    else if( 50 > minutes)
+    {
+      wordList.push_back(2); /* viertel */
+      wordList.push_back(7); /* vor */
+      hourWordIndex = 4; /* X+1 */
+    }
+    else if( 55 > minutes)
+    {
+      wordList.push_back(3); /* zehn */
+      wordList.push_back(7); /* vor */
+      hourWordIndex = 4; /* X+1 */
+    }
+    else if( 60 > minutes)
+    {
+      wordList.push_back(5); /* fuenf */
+      wordList.push_back(7); /* vor */
+      hourWordIndex = 4; /* X+1 */
+    }
+    else
+    {
+      abort();
+    }
+    return std::make_pair(wordList,hourWordIndex);
+  };
+  auto getHourWords = [](uint8_t hours, uint8_t minutes){
+    uint8_t theHour = hours;
+    if(25 >= minutes)
+    {
+      theHour += 1;
+    }
+    theHour = theHour%12;
+    int wordIndex = -1;
+    switch(theHour) {
+      case 0: wordIndex = 9; break; /* zwoelf */
+      case 1: wordIndex = 10; break; /* eins */
+      case 2: wordIndex = 11; break; /* zwei */
+      case 3: wordIndex = 12; break; /* drei */
+      case 4: wordIndex = 13; break; /* vier */
+      case 5: wordIndex = 14; break; /* fuenf */
+      case 6: wordIndex = 15; break; /* sechs */
+      case 7: wordIndex = 17; break; /* sieben */
+      case 8: wordIndex = 18; break; /* acht */
+      case 9: wordIndex = 19; break; /* neun */
+      case 10: wordIndex = 20; break; /* zehn */
+      case 11: wordIndex = 16; break; /* elf */
+      default: abort(); break;
+    }
+    return wordIndex;
+  };
+  auto getWords = [&getHourWords,&getMinuteWords](uint8_t hours, uint8_t minutes) {
+    auto hourResult = getHourWords(hours, minutes);
+    auto minuteResult = getMinuteWords(hours, minutes);
+    std::list<int> result;
+    for (auto wordIndex : minuteResult.first) {
+      result.push_back(wordIndex);
+    }
+    auto it = result.begin();
+    advance(it, minuteResult.second);
+    result.insert(it,hourResult);
+    return result;
+  };
+
+  update = getWords;
 
   /* xx:00 - xx:05 => Es ist x Uhr */
   /* xx:05 - xx:10 => Es ist fuenf nach x */
--- wordclock.h
+++ wordclock.h
@@ -3,16 +3,20 @@
 
 #include <QTime>
 #include <vector>
+#include <list>
+#include <functional>
 
 class WordClock
 {
 public:
   WordClock();
 private:
+  typedef std::list<int> WordList;
   std::vector<std::string> clockWords;
   void updateWords();
   void constructEnglish();
   void constructGerman();
+  std::function< WordList(uint8_t, uint8_t)> update;
 };
 
 #endif // WORDCLOCK_H
